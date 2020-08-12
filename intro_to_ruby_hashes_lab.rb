80b5fe111125805bb20b71db9aaa35237d448cf1
Scott Wilson <scottwilson1020@gmail.com>
2020-08-12 21:43:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,13 +19,12 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	   :railroads => 
-	    {
-	      :pieces =>4,
-	      :rent_in_dollars=> {},
-	      :names=>{}
-	    }
-	  
+		:railroads=>
+		{
+		:pieces=>4,
+		:names=>{},
+		:rent_in_dollars=>{}
+		}
 	}
 
 end
